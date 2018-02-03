f00f82b19c7943dddb0ff3803647d12c2e4f138b
tarokonno <tarokonno7@gmail.com>
2018-02-03 12:31:02 +1100

    create react app and publish to gh-pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="application">
+        <h1>Hello from React</h1>
+        <p>I was rendered from React</p>
+      </div>
+    );
+  }
+}
+
+export default App;
